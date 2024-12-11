099eee159a491d1062cecaa0b9ae7d29673ded5a
ramesh <vaka.ramesh@gmail.com>
2024-12-11 17:07:20 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,15 @@
+const heading = React.createElement("div",
+    {id: "parent"},[
+    React.createElement("div", {id: "child",},[
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", {id: "child",},[
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ])
+]);
+       
+    console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("hi"));
+root.render(heading);
